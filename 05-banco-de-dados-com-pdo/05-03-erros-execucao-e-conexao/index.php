ad785c88f38b16fe8a678753f2f19a0aed366d24
Yuri Monte <yuri.spm@gmail.com>
2021-08-06 11:56:01 -0300

    Inserindo Modulo 5

---
+++ 05-banco-de-dados-com-pdo/05-03-erros-execucao-e-conexao/index.php
@@ -0,0 +1,70 @@
+<?php
+require __DIR__ . '/../../fullstackphp/fsphp.php';
+fullStackPHPClassName("05.03 - Erros, conexão e execução");
+
+/*
+ * [ controle de erros ] http://php.net/manual/pt_BR/language.exceptions.php
+ */
+fullStackPHPClassSession("controle de erros", __LINE__);
+
+try{
+    //throw new PDOException("PDOException");
+
+    //throw new Exception("Exception");
+
+}catch (PDOException $exception){
+    var_dump($exception);
+}catch (Exception $exception){
+    echo "<p class = 'trigger error'>{$exception->getMessage()} </p>";
+}finally{
+    echo "<p class= 'triger'>Execução terminou!</p>";
+}
+
+/*
+ * [ php data objetc ] Uma classe PDO para manipulação de banco de dados
+ * http://php.net/manual/pt_BR/class.pdo.php
+ */
+fullStackPHPClassSession("php data objetc", __LINE__);
+
+try{
+
+    $pdo = new PDO(
+        "mysql:host=localhost;dbname=fullstackphp",
+        "phpmyadmin",
+        "d1n0ssaur0",
+        [
+            PDO:: MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+        ]
+    );
+
+    $stmt = $pdo->query ("SELECT * FROM users LIMIT 3");
+    while ($user = $stmt->fetch(PDO::FETCH_ASSOC)){
+        var_dump($user);
+    }
+
+
+}catch(PDOException $exception){
+    var_dump($exception);
+}
+
+
+/*
+ * [ conexão com singleton ] Conextar e obter um objeto PDO garantindo instância única
+ * http://br.phptherightway.com/pages/Design-Patterns.html
+ */
+fullStackPHPClassSession("conexão com singleton", __LINE__);
+
+require __DIR__."/../source/autoload.php";
+
+
+use Source\Database\Connect;
+$pdo1 = Connect::getInstance();
+$pdo2 = Connect::getInstance();
+
+var_dump(
+    $pdo1,
+    $pdo2,
+    Connect::getInstance(),
+    Connect::getInstance()::getAvailableDrivers(),
+    Connect::getInstance()->getAttribute(PDO::ATTR_DRIVER_NAME)
+);
---
+++ 05-banco-de-dados-com-pdo/source/Database/Connect.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Source\Database;
+
+use \PDO;
+use \PDOException;
+
+class Connect
+{
+    private const HOST = "localhost";
+    private const USER = "phpmyadmin";
+    private const DBNAME = "fsphp";
+    private const PASSWD = "7q5eb6eb@#";
+
+    private const OPTIONS = [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL
+    ];
+
+    private static $instance;
+
+    /**
+     * @return PDO
+     */
+    public static function getInstance(): PDO
+    {
+        if (empty(self::$instance)) {
+            try {
+                self::$instance = new PDO(
+                    "mysql:host=" . self::HOST . ";dbname=" . self::DBNAME,
+                    self::USER,
+                    self::PASSWD,
+                    self::OPTIONS
+                );
+            } catch (PDOException $exception) {
+                die("<h1>Whoops! Erro ao conectar...</h1>");
+            }
+        }
+
+        return self::$instance;
+    }
+
+    final private function __construct()
+    {
+    }
+
+    final private function __clone()
+    {
+    }
+}
